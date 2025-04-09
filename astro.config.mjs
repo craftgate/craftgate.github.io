22cfafc6712438eb5fbb9145d58323f47b2d986d
Muhammed Hasan Ince <hasan.ince@craftgate.io>
2025-04-09 13:31:43 +0300

    Update latest packages and upgrade tailwindcss@4 (#9)
    * Update latest packages
    
    * update tailwind and astro/tailwind
    
    * upgrade tailwind
    
    * format with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://craftgate.github.io',
-  integrations: [tailwind()],
+  vite: { plugins: [tailwindcss()] },
 });
